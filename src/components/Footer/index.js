056fb1371c468395b75416d111b599a231cc4528
adamalcantara <admajalcantara@icloud.com>
2021-05-22 18:58:12 -0400

    Added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer>
+            © Alcantara Development, 2021
+        </footer>
+    )
+}
+
+export default Footer;
