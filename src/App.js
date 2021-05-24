2287ae6cf6aadb8ceccaccb7644b45050489e0e8
adamalcantara <admajalcantara@icloud.com>
2021-05-23 21:30:32 -0400

    About to change EVERYTHING

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         </div>
 
 
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import andChill from "./and_chill.png"
 import "./style.css";
 
 function Home() {
     return (
-        <div>
-            <p>
-                "This is where the work section will go"
-            </p>
+        <div className="page">
+            <div className="container">
+               <div className="card" id="andChill">
+                   <div className="overlay">
+                        <div className="text">&Chill</div>
+                   </div>
+               </div>
+               <div className="card" id="cinepare">
+                   <div className="overlay">
+                        <div className="text">CinePare</div>
+                   </div>
+               </div>
+               <div className="card" id="weatherdash">
+                   <div className="overlay">
+                        <div className="text">Weather Dashboard</div>
+                   </div>
+               </div>
+               <div className="card" id="codequiz">
+                   <div className="overlay">
+                        <div className="text">Code Quiz</div>
+                   </div>
+               </div>
+               <div className="card">FIVE</div>
+               <div className="card">SIX</div>
+               <div className="card">SEVEN</div>
+               <div className="card">EIGHT</div>
+               <div className="card">NINE</div>
+               <div className="card">TEN</div>
+            </div>
         </div>
     )
 }
