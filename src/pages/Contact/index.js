ee9c4336caa93e78337de85d1eaafdac8c0a0590
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-07 16:50:10 -0400

    Styled form, added media queries

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./contactstyle.css";
 import ContactForm from "../../components/Contact"
 
 function Contact() {
@@ -9,7 +9,10 @@ function Contact() {
         // <p>Feel free to contact me with any questions you have about any projects you want to build.</p>
         // <a href="mailto: alcantaradevelopment@gmail.com">Click here to email me.</a>
         // </div>
+        <div className="contact">
+        <p>Use the form below to contact me. I look forward to hearing from you!</p>
         <ContactForm />
+        </div>
     )
 }
 
